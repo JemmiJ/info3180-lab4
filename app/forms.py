85e37238b7cbdb49eaf07c73dfd2bce48e4b04d2
JemmiJ <jemoiae@gmail.com>
2025-03-12 21:56:51 -0500

    Created and Tested Upload views and form

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired
 
@@ -7,3 +8,10 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
     submit = SubmitField('Login')
+
+class UploadForm(FlaskForm):
+    file = FileField('Upload an Image', validators=[
+        FileRequired(), 
+        FileAllowed(['jpg', 'png'], 'Just Images!')
+    ])
+    submit = SubmitField('Upload')
--- app/views.py
+++ app/views.py
@@ -1,11 +1,11 @@
 import os
 from app import app, db, login_manager
-from flask import render_template, request, redirect, url_for, flash, session, abort
+from flask import render_template, request, redirect, url_for, flash, session, abort, send_from_directory
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
-from werkzeug.security import check_password_hash
+from werkzeug.security import check_password_hash, generate_password_hash
 from app.models import UserProfile
-from app.forms import LoginForm
+from app.forms import LoginForm, UploadForm
 
 
 ###
@@ -25,18 +25,19 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
     # Instantiate your form class
-
+    form = UploadForm()
     # Validate file upload on submit
     if form.validate_on_submit():
         # Get file data and save to your uploads folder
-
+        file = form.file.data
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
-
-    return render_template('upload.html')
-
+    return render_template("upload.html", form=form)
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -70,8 +71,7 @@ def flash_errors(form):
         for error in errors:
             flash(u"Error in the %s field - %s" % (
                 getattr(form, field).label.text,
-                error
-), 'danger')
+                error), 'danger')
 
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
